4e476f49ce8920ccb22d5b271979dd557674ec8c
hyunsoolee <daniel.l@kakao.com>
2023-03-15 11:38:47 +0900

    Day 1

---
+++ Day_001/band-name-generator.py
@@ -0,0 +1,8 @@
+
+print("Welcome to the Band Name Generator.")
+
+your_city = input("What's name of the city you grew up in?\n")
+your_pet = input("What's your pet's name?")
+
+print(f"Your band name could be {your_city} {your_pet}")
+
