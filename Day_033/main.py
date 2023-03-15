dbc538a6e1344f6e62362e7ab0e884d436d08324
hyunsoolee <daniel.l@kakao.com>
2023-03-15 11:19:37 +0900

    Day 33

---
+++ Day_033/main.py
@@ -0,0 +1,31 @@
+import requests
+from datetime import datetime
+MY_LAT = 37.566536
+MY_LONG = 126.977966
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# latitude = data["iss_position"]["latitude"]
+# longitude = data["iss_position"]["longitude"]
+#
+# print(data)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+time_now = datetime.now()
+
+print(sunrise.split("T"))
+print(time_now)
