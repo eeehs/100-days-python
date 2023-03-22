e92599e6fa9f3144927e72197cf5b0f4d86ae8ce
hyunsoolee <daniel.l@kakao.com>
2023-03-22 10:45:56 +0900

    Day 6

---
+++ Day_006/main.py
@@ -0,0 +1,6 @@
+#  Reeborg's World 에서 코드 작성하며 프로젝트 진행 
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+
